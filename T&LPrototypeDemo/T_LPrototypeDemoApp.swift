63165d9a50f3bebc04bc700a7d22eff7bfbd3b7a
Moneeb S. Sayed <57688393+moneebssayed@users.noreply.github.com>
2022-01-07 15:22:26 -0500

    Initial Commit

---
+++ T&LPrototypeDemo/T_LPrototypeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  T_LPrototypeDemoApp.swift
+//  T&LPrototypeDemo
+//
+//  Created by Moneeb S. Sayed on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct T_LPrototypeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
